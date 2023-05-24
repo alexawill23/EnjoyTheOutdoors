438ae3ca5e5a943bef32ff863d3ad5addcd280e2
Alexa Williams <alexawill23@gmail.com>
2023-05-24 11:50:10 -0400

    changed some things for js national parks again

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -6,12 +6,20 @@ const locationRadio = document.getElementById("locationRadio");
 const parkRadio = document.getElementById("parkRadio");
 const searchBtn = document.getElementById("searchBtn");
 const locationDropdown = document.getElementById("locationDropdown");
+const parkDropdown = document.getElementById("parkDropdown"); 
+const nationalParksArray = [
+  // Start of National Parks Array Data 
+];
+const parkDetailsContainer = document.getElementById("parkDetailsContainer");
+const locationDetailsContainer = document.getElementById("locationDetailsContainer"); 
 
 window.onload = function () {
   // Radio button wire up
   console.log("window loaded");
   locationRadio.addEventListener("change", onLocationRadioChange);
   parkRadio.addEventListener("change", onParkRadioChange);
+  locationDropdown.addEventListener("change", onLocationDropdownChange);
+  parkDropdown.addEventListener("change", onParkDropdownChange); 
 };
 
 // Location Radio is Clicked
@@ -27,56 +35,48 @@ function onLocationRadioChange() {
   } else {
     locationDropdown.style.display = 'none';
   }
+  parkDetailsContainer.innerHTML = ''; // Clear park details when radio changes
 }
 
 // Park Radio is Clicked
 function onParkRadioChange() {
-    locationDropdown.style.display = 'none';
+  locationDropdown.style.display = 'none';
+  parkDetailsContainer.innerHTML = ''; // Clear park details when radio changes
+}
+
+// Location Dropdown Selection Changed
+function onLocationDropdownChange() {
+  const selectedLocation = locationDropdown.value;
+
+  // Filter national parks based on the selected location
+  const filteredParks = nationalParksArray.filter(park => nationalParksArray.LocationName === selectedLocation);
+
+  // Display the filtered parks
+  displayFilteredParks(filteredParks);
+}
+
+function displayFilteredParks(parks) {
+  parkDetailsContainer.innerHTML = ''; // Clear previous park details
+
+  // Display the filtered parks
+  for (let park of parks) {
+    const parkDetails = document.createElement("div");
+    parkDetails.classList.add("park-details");
+
+    const parkName = document.createElement("h3");
+    parkName.textContent = park.name;
+
+    const parkDescription = document.createElement("p");
+    parkDescription.textContent = park.description;
+
+    parkDetails.appendChild(parkName);
+    parkDetails.appendChild(parkDescription);
+    parkDetailsContainer.appendChild(parkDetails);
   }
+}; 
 
-  //Location Dropdown Selection Change 
-  function onLocationDropdownChange(){
-    const selectedLocation = locationDropdown.ariaValueMax; 
-    const filterParks = nationalParksArray.filter(park => park.LocationName === selectedLocation);
-    displayFilterParks(filterParks);  
-  }
+function onParkDropdownChange() {
 
+}
 
-// function onLocationRadioChange(){
-//     let currentLocation = locationsArray.value
-//     if(currentLocation == ""){
-//         hideParkDropdown();
-//     } else {
-//         hideLocationDropdown();
-//     }
-// }
-
-// populateLocationDropdown(locationsArray);
-
-//};
-
-// function controlRadioClick(){
-//     if (locationRadio.changed){
-//         locationsArray.style.display = 'block';
-//     } else if (parkRadio.changed){
-//         locationsArray.style.display = 'none';
-//     };
-// }
-// onParkRadioChange ()
-//     let currentPark = parkTypesArray.value
-
-// locationRadio.addEventListener('click', controlRadioClick);
-// parkRadio.addEventListener('click', controlRadioClick);
-
-// function shuffleStates(){
-// for (let state of nationalParksArray ){
-//     state == nationalParksArray.state;
-// }
-// };
-
-// //when Park Type Radio is Clicked
-// function parkRadioClicked (){
-// }
-
-// function shuffleParks(){
-// }
+// Rest of your code...
